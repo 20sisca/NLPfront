c706c3960c660c7eb2998fbebd5ddf0467e7f4b1
20sisca <alexis.siscaa@gmail.com>
2024-02-01 17:40:15 +0100

    button to anonymise text

--- src/Detail.js
+++ src/Detail.js
@@ -6,6 +6,7 @@ const Detail = () => {
   const location = useLocation();
   const fileData = location.state?.fileContent;
   const [responseMessage, setResponseMessage] = useState(null);
+  const [anonymizedText, setAnonymizedText] = useState(null);
   // Function to highlight names in the text
   const highlightNames = (text, names) => {
     return names.reduce((highlightedText, name) => {
@@ -30,6 +31,30 @@ const Detail = () => {
     }
   };
 
+  // Function to anonymize names in the text
+  const anonymizeNames = (text, names) => {
+    let anonymizedText = text;
+    names.forEach((name) => {
+      const regex = new RegExp(`\\b${name.replace(/\./g, '\\.')}\\b`, 'gi');
+      anonymizedText = anonymizedText.replace(regex, anonymizeSingleName(name));
+    });
+    return anonymizedText;
+  };
+
+  // Function to anonymize a single name
+  const anonymizeSingleName = (name) => {
+    // Anonymize the name while keeping the first letter
+    const firstLetter = name[0];
+    // const restOfName = '[D]'.repeat(name.length - 1);
+    return `[${firstLetter}]`;
+  };
+
+  // Handler for the anonymize button
+  const handleAnonymizeButtonClick = () => {
+    // Anonymize names in the text based on responseMessage
+    const anonymizedText = anonymizeNames(fileData, responseMessage);
+    setAnonymizedText(anonymizedText);
+  };
   return (
     <div className="detail-container">
       <header className="detail-header">
@@ -44,7 +69,7 @@ const Detail = () => {
       </div>
       <div className="action-button-container">
         <button className="action-button" onClick={handleButtonClick}>
-          Click me for action
+          Trouver les protagonistes
         </button>
       </div>
       {responseMessage && (
@@ -55,6 +80,12 @@ const Detail = () => {
             dangerouslySetInnerHTML={{ __html: highlightNames(fileData, responseMessage) }}
           />
 
+          <div className="action-button-container">
+            <button className="action-button" onClick={handleAnonymizeButtonClick}>
+              Anonymiser le texte
+            </button>
+          </div>
+          {anonymizedText}
         </div>
 
       )}    </div>
